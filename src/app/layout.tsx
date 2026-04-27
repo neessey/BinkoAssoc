5afb6aae3c1ebd6e2592ce1ba3fbd72c235bf913
Yaniss-Elie <yaniss123elie@gmail.com>
2026-04-27 13:47:25 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Cormorant_Garamond, Montserrat } from 'next/font/google'
 import './globals.css'
 import ClientBody from "./ClientBody";
+// import { Providers } from "./providers";
 
 
 const cormorant = Cormorant_Garamond({
@@ -49,7 +50,9 @@ export default function RootLayout({
     return (
         <html lang="fr">
             <body className={`${cormorant.variable} ${montserrat.variable} font-sans antialiased`}>
+                {/* <Providers> */}
                 <ClientBody>{children}</ClientBody>
+                {/* </Providers> */}
             </body>
         </html>
     )
